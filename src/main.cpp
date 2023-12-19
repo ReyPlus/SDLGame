645c4d4f09eb69245f231dfd777f4631ed1ae5bf
Rey Rodriguez <rey.rodriguez.plusplus@gmail.com>
2023-12-19 08:50:03 -0800

    Added Image class example use.

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,29 @@
 #include <iostream>
-#include "Base_Game.h"
 #include "SDL2/SDL.h"
+#include "SDL2/SDL_image.h"
+
+#include "Base_Game.h"
+#include "Image.h"
 #include "Graphics.h"
 #undef main
 
 class Game : public Base_Game {
 public:
-    Game(const char* name, unsigned int x, unsigned int y, unsigned int width, unsigned int height, Uint32 flags): lineColor(0,255,0), circleColor(255,255,255), rectColor(255,0,0), Base_Game(name, x, y, width, height, flags) {}
+    Game(const char* name, unsigned int x, unsigned int y, unsigned int width, unsigned int height, Uint32 SDLflags, int IMGflags): img("test.png", 0, 0, 200, 200, renderer, 30, SDL_FLIP_VERTICAL), Base_Game(name, x, y, width, height, SDLflags, IMGflags) {}
 
 private:
     void logic() {}
 
     void draw() {
-        //gfx->putPixel(100, 100, pixColor);
-        //gfx->putLine(0, 300, 800, 300, pixColor);
-        gfx->putLine(0, 250, 800, 250, lineColor);
-        gfx->putLine(0, 350, 800, 350, lineColor);
-        //gfx->putLine(400, 0, 400, 600, pixColor);
-        gfx->putLine(350, 0, 350, 600, lineColor);
-        gfx->putLine(450, 0, 450, 600, lineColor);
-        gfx->putRect(350, 250, 100, 100, rectColor);
-        gfx->putCircle(400, 300, 50, circleColor);
+        img.draw();
     }
 
 private:
-    Color circleColor;
-    Color rectColor;
-    Color lineColor;
+    Image img;
 };
 
 int main() {
-    Game* game = new Game("Example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_RESIZABLE);
-    Game::INIT();
+    Game* game = new Game("Example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_RESIZABLE, IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_WEBP);
 
     game->loop();
 
