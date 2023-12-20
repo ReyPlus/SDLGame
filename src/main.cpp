f641fc145a64f744a318e9480fc3e8192dc0398e
Rey Rodriguez <rey.rodriguez.plusplus@gmail.com>
2023-12-20 15:46:53 -0800

    Added sound/music example

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
 #include "SDL2/SDL.h"
 #include "SDL2/SDL_image.h"
+#include "SDL2/SDL_mixer.h"
 
 #include "Base_Game.h"
 #include "Image.h"
 #include "Graphics.h"
+#include "Sound.h"
 #undef main
 
 class Game : public Base_Game {
 public:
-    Game(const char* name, unsigned int x, unsigned int y, unsigned int width, unsigned int height, Uint32 SDLflags, int IMGflags): img("test.png", 0, 0, 200, 200, renderer, 30, SDL_FLIP_VERTICAL), Base_Game(name, x, y, width, height, SDLflags, IMGflags) {}
+    Game(const char* name, unsigned int x, unsigned int y, unsigned int width, unsigned int height, Uint32 windowFlags): effect("effect.wav"), img("test.png", 0, 0, 200, 200, renderer, 30, SDL_FLIP_VERTICAL), Base_Game(name, x, y, width, height, windowFlags) {
+        Music::load("music.mp3");
+    }
 
 private:
-    void logic() {}
+    void logic() {
+        if (kbd.checkPressed(SDL_SCANCODE_R)) effect.play();
+        if (kbd.checkHeld(SDL_SCANCODE_S)) {
+            Music::play();
+        }
+        else Music::pause();
+    }
 
     void draw() {
         img.draw();
@@ -20,10 +30,12 @@ private:
 
 private:
     Image img;
+    Effect effect;
 };
 
 int main() {
-    Game* game = new Game("Example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_RESIZABLE, IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_WEBP);
+    Game::INIT(SDL_INIT_EVERYTHING, IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_WEBP, MIX_INIT_MP3);
+    Game* game = new Game("Example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_RESIZABLE);
 
     game->loop();
 
