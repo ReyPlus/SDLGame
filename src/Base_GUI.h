63f35667635618f6b714ebed37a37f4a490bc75b
Rey Rodriguez <rey.rodriguez.plusplus@gmail.com>
2023-12-23 22:58:47 -0800

    Added Base_Container interface and Base_Clickable interface.

---
+++ src/Base_GUI.h
@@ -0,0 +1,28 @@
+#include "Graphics.h"
+
+class Base_Container {
+public:
+	Base_Container(Graphics* gfx);
+	Base_Container(int x, int y, int w, int h, Graphics* gfx);
+	virtual void modifyPos(int x, int y);
+	virtual void modifySize(int w, int h);
+	virtual void draw() = 0;
+protected:
+	SDL_Rect rect;
+	Graphics* gfx;
+};
+
+class Base_Clickable : public Base_Container {
+public:
+	Base_Clickable(Graphics* gfx);
+	Base_Clickable(int x, int y, int w, int h, Graphics* gfx);
+
+	virtual bool isHovering(int x, int y);
+	virtual void click() = 0;
+	virtual void draw() = 0;
+	virtual bool checkClicked(int x, int y);
+
+	virtual ~Base_Clickable() {};
+private:
+
+};
