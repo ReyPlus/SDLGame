61840a86ec3264a52619d4c8f7f44bded08bac45
Rey Rodriguez <rey.rodriguez.plusplus@gmail.com>
2023-12-24 07:09:45 -0800

    Progress bar and slider example.

--- src/main.cpp
+++ src/main.cpp
@@ -11,51 +11,86 @@
 #include "Font.h"
 #undef main
 
-class Button : public Base_Clickable {
+class HProgressBar : public Base_Bar {
 public:
-    Button(int x, int y, int w, int h, Color c, Graphics* gfx) : c(c), Base_Clickable(x, y, w, h, gfx) {}
-    void click() { printf("Button was Clicked\n"); }
-    void moveCentered(int x, int y) {
-        rect.x = int((float)x - (float)rect.w / 2);
-        rect.y = int((float)y - (float)rect.h / 2);
+    HProgressBar(int x, int y, int w, int h, Color filled, Color notFilled, Graphics* gfx, double start = 0.0, double min = 0.0, double max = 100.0, double step = 1.0) : filled(filled), notFilled(notFilled), Base_Bar(x, y, w, h, gfx, start, min, max, step) {}
+
+    void draw() {
+        gfx->putRect(rect, notFilled);
+        if (current > min) gfx->putRect(rect.x, rect.y, current*rect.w/max, rect.h, filled);
     }
-    void draw() { gfx->putRect(rect, c); }
 private:
-    Color c;
+    Color filled;
+    Color notFilled;
 };
 
-class Switch : public Base_Switch {
+class HSlider : public Base_Slider {
 public:
-    Switch(int x, int y, int w, int h, Graphics* gfx) : offC(224, 79, 79), onC(74, 119, 224), Base_Switch(x, y, w, h, gfx) {}
+    HSlider(int x, int y, int w, int h, Color filled, Color notFilled, Graphics* gfx, double start = 0.0, double min = 0.0, double max = 100.0, double step = 1.0) : filled(filled), notFilled(notFilled), handle(x + start * rect.w / max, y - 5, 10, h + 10, gfx), Base_Slider(x, y, w, h, gfx, start, min, max, step) {}
+
     void draw() {
-        gfx->putRect(rect, switched? onC : offC);
+        gfx->putRect(rect, notFilled);
+        if (current > min) gfx->putRect(rect.x, rect.y, current * rect.w / max, rect.h, filled);
+        handle.draw();
     }
+
+    bool isDragged(int x, int y) {
+        if (handle.isHovering(x, y) && x >= rect.x && x <= rect.x + rect.w) {
+            handle.modifyXCentered(x);
+            setCurrent(x - rect.x);
+            return true;
+        }
+
+        return false;
+    }
+
+    bool isClicked(int x, int y) { return false; }
 private:
-    Color offC;
-    Color onC;
+    class Handle : public Base_Container {
+    public:
+        Handle(int x, int y, int w, int h, Graphics* gfx): Base_Container(x, y, w, h, gfx) {}
+        void draw() { gfx->putRect(rect, {255,255,255}); }
+        void modifyXCentered(int x) {
+            modifyPos(x - rect.w/2, rect.y);
+        }
+        bool isHovering(int x, int y) {
+            return (x <= rect.x + rect.w) && (x >= rect.x) && (y <= rect.y + rect.h) && (y >= rect.y);
+        }
+    private:
+
+    };
+
+    Color filled;
+    Color notFilled;
+    Handle handle;
 };
 
 class Game : public Base_Game {
 public:
-    Game(const char* name, unsigned int x, unsigned int y, unsigned int width, unsigned int height, Uint32 windowFlags) : mySwitch(0,0,100,50, gfx), greenButton(0, 0, 100, 50, { 84, 214, 81 }, gfx), Base_Game(name, x, y, width, height, windowFlags) {
-        greenButton.moveCentered(width/2, height/2);
+    Game(const char* name, unsigned int x, unsigned int y, unsigned int width, unsigned int height, Uint32 windowFlags) : bar(20, 5, 100, 20, { 77, 185, 247 }, { 13, 78, 115 }, gfx, 20.0, 0.0, 100.0, 5.0), slider(20, 40, 100, 20, { 77, 185, 247 }, { 13, 78, 115 }, gfx, 20.0, 0.0, 100.0, 5.0), Base_Game(name, x, y, width, height, windowFlags) {
+        
     }
 private:
     void logic() {
-        if (mouse.leftPressed()) {
-            greenButton.checkClicked(mouse.x, mouse.y);
-            mySwitch.checkClicked(mouse.x, mouse.y);
+        if (kbd.checkPressed(SDL_SCANCODE_UP)) {
+            bar.takeSteps(1);
         }
+
+        if (kbd.checkPressed(SDL_SCANCODE_DOWN)) {
+            bar.takeSteps(-1);
+        }
+
+        if (mouse.leftHeld) slider.isDragged(mouse.x, mouse.y);
     }
 
     void draw() {
-        greenButton.draw();
-        mySwitch.draw();
+        bar.draw();
+        slider.draw();
     }
 
 private:
-    Switch mySwitch;
-    Button greenButton;
+    HProgressBar bar;
+    HSlider slider;
 };
 
 int main() {
